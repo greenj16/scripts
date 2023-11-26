9c29d3dae7f9b0dd9fb23b4164f81ffb09b8daf5
greenj16 <iam.jtg1216@gmail.com>
2023-11-25 20:40:25 -0500

    new

--- wazuh.sh
+++ wazuh.sh
@@ -2,4 +2,18 @@
 
 . IPs.sh
 
-echo $ubu_web
+current_ip=$(hostname -I | awk '{print $1}')
+
+if [ "$ubu_web" == "$current_ip" ]; then
+
+sed -i 's/#\?\(vm.max_map_count=\s*\).*$/vm.max_map_count=262144/' /etc/sysctl.conf
+
+
+
+
+
+
+else
+    echo "please run this on the Ubuntu Web host..."
+
+fi
