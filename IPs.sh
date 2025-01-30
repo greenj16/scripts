c8e9ddd36acdfb2ab3fe594d17bb7dd3f30e6bb6
John Green <67358545+greenj16@users.noreply.github.com>
2025-01-30 11:50:21 -0500

    Update IPs.sh

--- IPs.sh
+++ IPs.sh
@@ -6,9 +6,10 @@ YELLOW=`tput setaf 3`                       # code for yellow text
 ORANGE=`tput setaf 214`                     # code for orange text
 WHITE=`tput setaf 7`                        # code for white text
 NC=`tput sgr0`                              # Reset the text color
-echo "      ${ORANGE}/\ ${NC}"
-        echo "    ${ORANGE}_/./ ${NC}"
-        echo "${ORANGE} ,-${RED}'    ${$ORANGE}\`${RED}-:..${ORANGE}-'/ ${NC}"
-        echo "${ORANGE}: ${GREEN}o ${RED})      _  ${ORANGE}("
-        echo "${ORANGE}\"\`-..${RED}..,${ORANGE}--${RED}; \`${ORANGE}-.\ "
-        echo "${ORANGE}    \`${RED}'\""
+        echo "${GREEN}You are not Ubuntu Workstation...have a fish instead${NC}"
+        echo "      ${YELLOW}/\ ${NC}"
+        echo "    ${YELLOW}_/./ ${NC}"
+        echo "${YELLOW} ,-${RED}'    ${YELLOW}\`${RED}-:..${YELLOW}-'/ ${NC}"
+        echo "${YELLOW}: ${GREEN}o ${RED})      _  ${YELLOW}("
+        echo "${YELLOW}\"\`-..${RED}..,${YELLOW}--${RED}; \`${YELLOW}-.\ "
+        echo "${YELLOW}    \`${RED}'\""
