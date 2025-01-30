9af4ca5d7b1c4677dcfa003d583a456a4f123aa5
John Green <67358545+greenj16@users.noreply.github.com>
2025-01-30 11:42:43 -0500

    Update IPs.sh

--- IPs.sh
+++ IPs.sh
@@ -1,6 +1,13 @@
+
+RED=`tput setaf 1`                          # code for red console text
+GREEN=`tput setaf 2`                        # code for green text
+YELLOW=`tput setaf 3`                       # code for yellow text
+ORANGE=`tput setaf 214`                     # code for orange text
+WHITE=`tput setaf 7`                        # code for white text
+NC=`tput sgr0`                              # Reset the text color
 echo "      ${ORANGE}/\ ${NC}"
         echo "    ${ORANGE}_/./ ${NC}"
-        echo "${ORANGE} ,-${RED}'    {$ORANGE}\`${RED}-:..${ORANGE}-'/ ${NC}"
+        echo "${ORANGE} ,-${RED}'    ${$ORANGE}\`${RED}-:..${ORANGE}-'/ ${NC}"
         echo "${ORANGE}: ${GREEN}o ${RED})      _  ${ORANGE}("
         echo "${ORANGE}\"\`-..${RED}..,${ORANGE}--${RED}; \`${ORANGE}-.\ "
         echo "${ORANGE}    \`${RED}'\""
