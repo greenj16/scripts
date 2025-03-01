7b618b95f19240cc76c97761bc3e83afc4ed4710
John Green <67358545+greenj16@users.noreply.github.com>
2025-03-01 18:59:56 -0500

    Add files via upload

--- linMon.sh
+++ linMon.sh
@@ -18,263 +18,6 @@ update_process_list() {
 watch -n 2 update_process_list &
 watch_pid=$!
 
-'''
-display_proc() {
-
-    while true; do
-        clear
-
-        count=0
-        # reads the new process file
-        while IFS= read -r line; do
-            # skips the first line
-            if [ count -eq 0 ]; then
-                echo "Line num, PID, PPID, CMD"
-                continue
-            else
-                echo "${count}: ${line}"
-            fi
-
-            ((count++))
-            sleep 2
-        done < "/tmp/new_processes"
-    done
-}
-'''
-
-'''
-# Continuously prompt the user for input
-while_loop=true
-while $while_loop; do
-
-    echo "################################"
-    echo "# ${GREEN}1 Investigate${NC}                #"
-    echo "# ${GREEN}2 Eradicate${NC}                  #"
-    echo "# ${GREEN}3 Investigate and Eradicate${NC}  #"
-    echo "################################"
-    echo ""
-    read -p "${GREEN}Enter choose an option (type '${RED}exit${GREEN}' to quit): ${NC}" user_input
-
-
-    case $user_input in
-        "1")
-            clear
-            read -p "${GREEN}Enter the PID to investigate: ${NC}" pid_input
-
-            ps -co pid,cmd,user,lstart > /tmp/t.txt
-
-            process=()
-            while IFS= read -r line; do
-                IFS=' ' read -r -a line_array <<< "$line"
-                if [[ $line_array[0] -eq $pid_input ]]; then
-
-                    cmd="${line_array[1]}"
-                    ps_user="${line_array[2]}"
-                    location=$(find / -name $cmd)
-                    time_start="${line_array[3]}"
-                    cmd_line=$(ps -o cmd,pid | grep $pid_input | awk '{print $1}')
-
-                    netstat_line=$(netstat -peanut | grep $pid_input)
-                    if [[ $netstat_line -eq "" ]]; then
-                        listen_state="This process is not using network connections"
-                        dip="This process is not using network connections"
-                    else
-                        listen_state=$(echo "$netstat_line" | awk '{print $6}')
-                        dip=$(echo "$netstat_line" | awk '{print $5}')
-                    fi
-
-                    if [[ $listen_state -eq "LISTEN" ]]; then
-
-                        clear
-                        echo "${GREEN}Command used: ${YELLOW}${cmd_line}{$NC}"
-                        echo "${GREEN}Where app is located: ${YELLOW}${location}${NC}"
-                        echo "${GREEN}Who ran it: ${YELLOW}${ps_user}${NC}"
-                        echo "${GREEN}Start time: ${YELLOW}${time_start}${NC}"
-                        if [[ $listen_state -eq "ESTABLISHED" ]]; then
-                            echo "${GREEN}Process network status: ${RED}${listen_state}${NC}"
-                            echo "${GREEN}Destination address: ${RED}${dip}${NC}"
-                            echo ""
-                            echo "${RED}CONNECTION ESTABLISHED${GREEN} - Check logs for malicous activity from ${RED}${dip}${NC}"
-                            echo "${GREEN}Please use the 'Eradicate' option [enter '2'] if this is confirmed malicous${NC}"
-                            echo ""
-                            echo ""
-                        else
-                            echo "${GREEN}Process network status: ${YELLOW}${listen_state}${NC}"
-                            echo "${GREEN}Destination address: ${YELLOW}${dip}${NC}"
-                            echo ""
-                            echo "${GREEN}Please use the 'Eradicate' option [enter '2'] if this is confirmed malicous${NC}"
-                            echo ""
-                            echo ""
-                        fi
-                fi
-            done < "/tmp/t.txt"
-
-            rm /tmp/t.txt
-            ;;
-        "2")
-            clear
-            read -p "${GREEN}Enter the PID to eradicate: ${NC}" pid_input
-            clear
-
-            ps_output=$(ps -co pid,cmd,user,lstart | grep $pid_input)
-            cmd=$(echo "$ps_output" | awk '{print $2}')
-            location=$(find / -name $cmd)
-
-            IFS=' ' read -r -a locations <<< "$location"
-            for index in "${!locations[@]}"; do
-                echo "${GREEN}All locations of PUP:${NC}"
-                echo "${index}: ${YELLOW}${locations[$index]}${NC}"
-            done 
-
-            echo ""
-            bool=true
-            while $bool; do
-                read -p "${GREEN}Enter the number of the location to move, enter 'none', or enter 'all': ${NC}" location_input
-                case $user_input in
-                    "none")
-                        echo "No file locations moved..."
-                        $bool=false
-                        ;;
-                    "all")
-                        for index in "${!locations[@]}"; do
-                            echo "Moving all files to /var/zds/ ..."
-                            mv "${locations[$index]}" "/var/zds/${cmd}_${index}"
-                        done 
-                        $bool=false
-                        ;;
-                    *)
-                        IFS=' ' read -r -a indexes <<< "${!locations[@]}"
-                        for element in "${indexes[@]}"; do
-                            if [ "$element" == "$user_input" ]; then
-                                echo "Moving ${locations[$user_input]} to /var/zds ..."
-                                mv "${locations[$index]}" "/var/zds/${cmd}_${user_input}"
-                                $bool=false
-                            else    
-                                echo "Invalid response..."
-                                clear
-                            fi
-                        done
-                        ;;
-                esac
-            done
-
-            echo "${RED}Killing PID=${YELLOW}${pid_input}${RED}...${NC}"
-            kill -9 $pid_input
-
-            ;;
-        "3")
-            clear
-            read -p "${GREEN}Enter the PID to investigate: ${NC}" pid_input
-
-            ps -co pid,cmd,user,lstart > /tmp/t.txt
-
-            process=()
-            while IFS= read -r line; do
-                IFS=' ' read -r -a line_array <<< "$line"
-                if [[ $line_array[0] -eq $pid_input ]]; then
-
-                    cmd="${line_array[1]}"
-                    ps_user="${line_array[2]}"
-                    location=$(find / -name $cmd)
-                    time_start="${line_array[3]}"
-                    cmd_line=$(ps -o cmd,pid | grep $pid_input | awk '{print $1}')
-
-                    netstat_line=$(netstat -peanut | grep $pid_input)
-                    if [[ $netstat_line -eq "" ]]; then
-                        listen_state="This process is not using network connections"
-                        dip="This process is not using network connections"
-                    else
-                        listen_state=$(echo "$netstat_line" | awk '{print $6}')
-                        dip=$(echo "$netstat_line" | awk '{print $5}')
-                    fi
-
-                    if [[ $listen_state -eq "LISTEN" ]]; then
-
-                        clear
-                        echo "${GREEN}Command used: ${YELLOW}${cmd_line}{$NC}"
-                        echo "${GREEN}Where app is located: ${YELLOW}${location}${NC}"
-                        echo "${GREEN}Who ran it: ${YELLOW}${ps_user}${NC}"
-                        echo "${GREEN}Start time: ${YELLOW}${time_start}${NC}"
-                        if [[ $listen_state -eq "ESTABLISHED" ]]; then
-                            echo "${GREEN}Process network status: ${RED}${listen_state}${NC}"
-                            echo "${GREEN}Destination address: ${RED}${dip}${NC}"
-                            echo ""
-                            echo "${RED}CONNECTION ESTABLISHED${GREEN} - Check logs for malicous activity from ${RED}${dip}${NC}"
-                            echo ""
-                            echo ""
-                        else
-                            echo "${GREEN}Process network status: ${YELLOW}${listen_state}${NC}"
-                            echo "${GREEN}Destination address: ${YELLOW}${dip}${NC}"
-                            echo ""
-                            echo ""
-                        fi
-                fi
-            done < "/tmp/t.txt"
-
-            rm /tmp/t.txt
-            
-            ps_output=$(ps -co pid,cmd,user,lstart | grep $pid_input)
-            cmd=$(echo "$ps_output" | awk '{print $2}')
-            location=$(find / -name $cmd)
-
-            IFS=' ' read -r -a locations <<< "$location"
-            for index in "${!locations[@]}"; do
-                echo "${GREEN}All locations of PUP:${NC}"
-                echo "${index}: ${YELLOW}${locations[$index]}${NC}"
-            done 
-
-            echo ""
-            bool=true
-            while $bool; do
-                read -p "${GREEN}Enter the number of the location to move, enter 'none', or enter 'all': ${NC}" location_input
-                case $user_input in
-                    "none")
-                        echo "No file locations moved..."
-                        $bool=false
-                        ;;
-                    "all")
-                        for index in "${!locations[@]}"; do
-                            echo "Moving all files to /var/zds/ ..."
-                            mv "${locations[$index]}" "/var/zds/${cmd}_${index}"
-                        done 
-                        $bool=false
-                        ;;
-                    *)
-                        IFS=' ' read -r -a indexes <<< "${!locations[@]}"
-                        for element in "${indexes[@]}"; do
-                            if [ "$element" == "$user_input" ]; then
-                                echo "Moving ${locations[$user_input]} to /var/zds ..."
-                                mv "${locations[$index]}" "/var/zds/${cmd}_${user_input}"
-                                $bool=false
-                            else    
-                                echo "Invalid response..."
-                                clear
-                            fi
-                        done
-                        ;;
-                esac
-            done
-
-            echo "${RED}Killing PID=${YELLOW}${pid_input}${RED}...${NC}"
-            kill -9 $pid_input
-
-            ;;
-        "exit")
-            while_loop=false
-            ;;
-        *)
-            echo "${YELLOW}Invalid response...${NC}"
-            ;;
-    esac
-
-    bool=true
-    while $bool; do
-        read -p "${GREEN}Press enter to continue...${NC}" x
-        bool=false
-    done
-done
-exit
-'''
 
 # Start a new tmux session
 tmux new-session -d -s my_session
@@ -283,7 +26,25 @@ tmux new-session -d -s my_session
 tmux split-window -v
 
 # Run the echo script in the top pane
-tmux send-keys -t my_session.0 'display_proc() { while true; do clear; count=0; while IFS= read -r line; do if [ $count -eq 0 ]; then echo "PID, PPID, CMD"; ((count++)); continue; else echo "${count}: ${line}"; fi; ((count++)); sleep 2; done < "/tmp/new_processes"; done; }' C-m
+tmux send-keys -t my_session.0 "
+    display_proc() {
+        while true; do
+            clear
+            count=0
+            # reads the new process file
+            while IFS= read -r line; do
+                # skips the first line
+                if [ \$count -eq 0 ]; then
+                    echo \"Line num, PID, PPID, CMD\"
+                else
+                    echo \"\${count}: \${line}\"
+                fi
+                ((count++))
+            done < \"/tmp/new_processes\"
+            sleep 2
+        done
+    }
+" C-m
 tmux send-keys -t my_session.0 'display_proc' C-m
 
 # Allow user input in the bottom pane
